31f6f37560b60aa1c357d3be900534507b7c881b
Simon Gunacker <simon-gunacker@users.noreply.github.com>
2023-03-06 07:38:26 +0000

    Übung: len implementieren

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+def stöln(xs):
+  for i, _ in enumerate(xs):
+    pass
+  return i + 1
+
+
 todos = ["FSST", "Mathe", "Deutsch", "HWE"]
 
 index = int(input("Welchen Wert ändern? ")) - 1
@@ -5,10 +11,10 @@ index = int(input("Welchen Wert ändern? ")) - 1
 todos[index] = input("Neuer Wert: ")
 
 i = 0 
-while i < len(todos):
+while i < stöln(todos):
   print(f"{i+1}. {todos[i]}")
   i = i + 1
 
 
-for i, todo in enumerate(todos, start=1):
-  print(f"{i}. {todo}")
+#for i, todo in enumerate(todos, start=1):
+#  print(f"{i}. {todo}")
