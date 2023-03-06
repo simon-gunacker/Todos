b26710f4692eb1d666f698830d3066e290d5e98b
Simon Gunacker <simon-gunacker@users.noreply.github.com>
2023-03-06 08:13:37 +0000

    Todo und Status

--- main.py
+++ main.py
@@ -4,17 +4,28 @@ def stöln(xs):
   return i + 1
 
 
-todos = ["FSST", "Mathe", "Deutsch", "HWE"]
+todos = [
+  "FSST", 
+  "Abgeschlossen", 
+  "Mathe", 
+  "Offen", 
+  "Deutsch", 
+  "Verschoben", 
+  "HWE", 
+  "Offen"
+]
 
-index = int(input("Welchen Wert ändern? ")) - 1
+i = 0
+while i < len(todos):
+  print(f"{todos[i]:30} {todos[i+1]}")
+  i = i + 2
 
-todos[index] = input("Neuer Wert: ")
 
-i = 0 
-while i < stöln(todos):
-  print(f"{i+1}. {todos[i]}")
-  i = i + 1
 
+#index = int(input("Welchen Wert ändern? ")) - 1
+
+#todos[index] = input("Neuer Wert: ")
 
 #for i, todo in enumerate(todos, start=1):
-#  print(f"{i}. {todo}")
+#  print(f"{i:02}. {todo:30} [X]")
+
